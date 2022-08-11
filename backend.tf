2de3959e179022d933952e97472391720e399629
SuryenduB <suryendubbhattacharyya@gmail.com>
2022-08-11 21:08:21 +0530

    backend.tf modified

--- backend.tf
+++ backend.tf
@@ -3,29 +3,7 @@ terraform {
     resource_group_name   = "MultiForest-Home-In-World_2"
     storage_account_name  = "az500sury"
     container_name        = "az500sury"
-    key                   = "terraform.tfstate"
+    key                   = "prod.terraform.tfstate"
   }
 }
 
-provider "azurerm" {
-  subscription_id =     var.subscription_id
-  client_id       =     var.client_id
-  client_secret   =     var.client_secret
-  tenant_id       =     var.tenant_id
-  alias  = "backend"
-  
-  features {}
-}
-
-variable "subscription_id" {
-  default = ""
-}
-variable "client_id" {
-  default = ""
-}
-variable "client_secret" {
-  default = ""
-}
-variable "tenant_id" {
-  default = ""
-}
