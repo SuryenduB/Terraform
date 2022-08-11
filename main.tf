1643abbfe797d78db4b6ee5e981db1c9552eadff
SuryenduB <suryendubbhattacharyya@gmail.com>
2022-08-11 20:00:58 +0530

    Variable added in Main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,22 @@
 provider "azurerm" {
+  subscription_id =     var.subscription_id
+  client_id       =     var.client_id
+  client_secret   =     var.client_secret
+  tenant_id       =     var.tenant_id
   
   features {}
+}
+variable "subscription_id" {
+  default = ""
+}
+variable "client_id" {
+  default = ""
+}
+variable "client_secret" {
+  default = ""
+}
+variable "tenant_id" {
+  default = ""
 }
 
 resource "azurerm_resource_group" "example" {
