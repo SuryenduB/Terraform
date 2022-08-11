c4f5690f57b76219a2ab72893b6261be1d945e20
SuryenduB <suryendubbhattacharyya@gmail.com>
2022-08-11 21:54:37 +0530

    commenting out backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "MultiForest-Home-In-World_2"
-    storage_account_name  = "az500sury"
-    container_name        = "az500sury"
-    key                   = "prod.terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name   = "MultiForest-Home-In-World_2"
+#     storage_account_name  = "az500sury"
+#     container_name        = "az500sury"
+#     key                   = "prod.terraform.tfstate"
+#   }
+# }
 
