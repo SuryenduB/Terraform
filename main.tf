66291c413610f12174da6d4e4581c69c157d4e77
SuryenduB <suryendubbhattacharyya@gmail.com>
2022-08-16 11:10:33 +0530

    Format change

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #   client_id       =     var.client_id
 #   client_secret   =     var.client_secret
 #   tenant_id       =     var.tenant_id
-  
+
 #   features {}
 # }
 
@@ -75,55 +75,110 @@
 # }
 
 module "virtualnachine" {
-  source = "./virtualmachine"
+  source        = "./virtualmachine"
   resourcename  = "AzureRMResource"
-  diagnostic = "bootmachinediag"
-  env = "dev"
-location      = "North Europe"
-tags          = { enviornment = "demo", owner = "shan", purpose = "TFdemo" }
-storagename   = "azurermtfdemoshan"
-containername = "tfdemocontainer"
-dnsname       = ["shan.com", "shan1.com", "shan2.com", "shan3.com"]
-networkrule = [
-  {
-    name                       = "test123"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"  
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  },
-  {
-    name                       = "test1234"
-    priority                   = 101
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "443"
-    destination_port_range     = "*"   
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  },
-  {
-    name                       = "test1235"
-    priority                   = 102
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "3389"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  },
-]
-tag2 = {resource="virtualmachine",costcentre = "demoyfcourse"}
-enviornment = "sandbox"
-account_type = "Standard_GRS"
-loc = ["east","us"]
-address_space = ["10.0.0.0/16","10.0.0.1/32","10.0.0.1/24","10.0.2.0/23"] 
+  diagnostic    = "bootmachinediag"
+  env           = "dev"
+  location      = "North Europe"
+  tags          = { enviornment = "demo", owner = "shan", purpose = "TFdemo" }
+  storagename   = "azurermtfdemoshan"
+  containername = "tfdemocontainer"
+  dnsname       = ["shan.com", "shan1.com", "shan2.com", "shan3.com"]
+  networkrule = [
+    {
+      name                       = "test123"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "test1234"
+      priority                   = 101
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "443"
+      destination_port_range     = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "test1235"
+      priority                   = 102
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "3389"
+      destination_port_range     = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+  ]
+  tag2          = { resource = "virtualmachine", costcentre = "demoyfcourse" }
+  enviornment   = "sandbox"
+  account_type  = "Standard_GRS"
+  loc           = ["east", "us"]
+  address_space = ["10.0.0.0/16", "10.0.0.1/32", "10.0.0.1/24", "10.0.2.0/23"]
+
+
+}
+
+module "vmprod" {
+  source        = "./virtualmachine"
+  resourcename  = "AzureRMResource"
+  diagnostic    = "bootmachinediag"
+  env           = "prod"
+  location      = "North Europe"
+  tags          = { enviornment = "prod", owner = "shan", purpose = "TFdemo" }
+  storagename   = "azurermtfdemoshantf"
+  containername = "tfdemocontainertf"
+  dnsname       = ["shan.com", "shan1.com", "shan2.com", "shan3.com"]
+  networkrule = [
+    {
+      name                       = "test123"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "test1234"
+      priority                   = 101
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "443"
+      destination_port_range     = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "test1235"
+      priority                   = 102
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "3389"
+      destination_port_range     = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+  ]
+  tag2          = { resource = "virtualmachine", costcentre = "demoyfcourse" }
+  enviornment   = "sandbox"
+  account_type  = "Standard_GRS"
+  loc           = ["east", "us"]
+  address_space = ["10.0.0.0/16", "10.0.0.1/32", "10.0.0.1/24", "10.0.2.0/23"]
+
 
 
 }
