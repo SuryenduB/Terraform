1bb470e9ef47080105418bf28ce4ee1bb5c5e1cf
SuryenduB <suryendubbhattacharyya@gmail.com>
2022-09-16 22:38:21 +0530

    Cross references

---
+++ AWS/reference.tf
@@ -0,0 +1,25 @@
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.web.id
+  allocation_id = aws_eip.lb.id
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tcp"
+  description = "Allow Tcpinbound traffic"
+  
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["${aws_eip.lb.public_ip}/32"]
+    
+  }
+
+  
+
+  tags = {
+    Name = "allow_tcp"
+  }
+}
