318f35370be0d63af8ddbd5d846907f740066e63
SuryenduB <suryendubbhattacharyya@gmail.com>
2022-12-03 04:03:46 +0530

    backend

---
+++ AWS/backend.tf
@@ -0,0 +1,9 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "suryendu-tfstate-backend"
+#     key    = "remotedemo.tfstate"
+#     region = "us-west-2"
+#     access_key = "AKIA3WFERZ2IMP4AGGHJ"
+#     secret_key = ""
+#   }
+# }
