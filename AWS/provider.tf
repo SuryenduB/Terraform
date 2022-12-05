6d7f54461236e428e8efd7e46d9a39d8d463eebb
SuryenduB <suryendubbhattacharyya@gmail.com>
2022-12-06 01:16:13 +0530

    Provider Alias

--- AWS/provider.tf
+++ AWS/provider.tf
@@ -1,17 +1,16 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIA3WFERZ2IMP4AGGHJ"
+  access_key = "AKIA3WFERZ2IA5GBFPML"
   secret_key = var.secret_key
 
 }
 
-#TF_VAR_secret_key is the Secret key defined in global variable. $env:TF_VAR_secret_key
 
 
 provider "aws" {
   alias      = "southeast"
   region     = "ap-southeast-1"
-  access_key = "AKIA3WFERZ2IMP4AGGHJ"
+  access_key = "AKIA3WFERZ2IA5GBFPML"
   secret_key = var.secret_key
 
 }
