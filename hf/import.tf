b0679445a57d99bada494ab15042349b4e4fcec5
Suryendu <suryendubbhattacharyya@gmail.com>
2023-09-15 11:54:45 +0200

    Added Reference to the staging NSG

--- hf/import.tf
+++ hf/import.tf
@@ -1,4 +1,4 @@
 import {
     to = azurerm_network_security_rule.AllowLDAPsInboundWhitelist
-    id = "/subscriptions/22179b5a-92dc-4268-a5fb-7889d59f61ce/resourceGroups/IAM-PRD-RSG/providers/Microsoft.Network/networkSecurityGroups/IAM-PRD-NSG/securityRules/AllowLDAPsInboundWhitelist"
+    id = "/subscriptions/22179b5a-92dc-4268-a5fb-7889d59f61ce/resourceGroups/IAM-STG-RSG/providers/Microsoft.Network/networkSecurityGroups/IAM-STG-NSG/securityRules/NSGRuleLDAP"
 }
--- hf/new.tf
+++ hf/new.tf
@@ -1,22 +1,23 @@
 # __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
 
-
-
+# __generated__ by Terraform
 resource "azurerm_network_security_rule" "AllowLDAPsInboundWhitelist" {
   access                                     = "Allow"
-  description                                = null
+  description                                = "Allow LDAP  (Home Lab) 46.114.180.35"
   destination_address_prefix                 = null
-  destination_address_prefixes               = ["10.11.11.5", "10.11.11.6"]
+  destination_address_prefixes               = ["10.0.0.4", "10.0.0.5"]
   destination_application_security_group_ids = []
-  destination_port_range                     = "636"
+  destination_port_ranges                    = ["389", "7389"]
   direction                                  = "Inbound"
-  name                                       = "AllowLDAPsInboundWhitelist"
-  network_security_group_name                = "IAM-PRD-NSG"
-  priority                                   = 1002
+  name                                       = "NSGRuleLDAP"
+  network_security_group_name                = "IAM-STG-NSG"
+  priority                                   = 2010
   protocol                                   = "*"
-  resource_group_name                        = "IAM-PRD-RSG"
+  resource_group_name                        = "IAM-STG-RSG"
   source_address_prefix                      = null
-  source_address_prefixes                    = ["13.236.164.39", "168.63.129.16", "212.91.244.2", "34.241.251.84", "34.248.252.181", "52.48.110.57", "52.48.92.0", "54.206.155.24", "85.239.123.201"]
+  source_address_prefixes                    = ["13.79.85.90", "20.238.65.61", "212.91.244.2", "34.254.60.147", "46.114.180.35", "52.48.92.0", "77.247.82.165", "85.239.123.201","52.215.45.1"]
   source_application_security_group_ids      = []
   source_port_range                          = "*"
+  
 }
