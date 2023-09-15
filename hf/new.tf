c6ade7353f88c05993a83b6d91bc2e06317f8ed6
Suryendu <suryendubbhattacharyya@gmail.com>
2023-09-15 12:03:48 +0200

    Corrected a bug as the rule should point to LDAPS rule

--- hf/new.tf
+++ hf/new.tf
@@ -8,16 +8,17 @@ resource "azurerm_network_security_rule" "AllowLDAPsInboundWhitelist" {
   destination_address_prefix                 = null
   destination_address_prefixes               = ["10.0.0.4", "10.0.0.5"]
   destination_application_security_group_ids = []
-  destination_port_ranges                    = ["389", "7389"]
+  destination_port_range                     = null
+  destination_port_ranges                    = ["636", "7636"]
   direction                                  = "Inbound"
-  name                                       = "NSGRuleLDAP"
+  name                                       = "NSGRuleLDAPS"
   network_security_group_name                = "IAM-STG-NSG"
-  priority                                   = 2010
+  priority                                   = 2020
   protocol                                   = "*"
   resource_group_name                        = "IAM-STG-RSG"
   source_address_prefix                      = null
   source_address_prefixes                    = ["13.79.85.90", "20.238.65.61", "212.91.244.2", "34.254.60.147", "46.114.180.35", "52.48.92.0", "77.247.82.165", "85.239.123.201","52.215.45.1"]
   source_application_security_group_ids      = []
   source_port_range                          = "*"
-  
+
 }
