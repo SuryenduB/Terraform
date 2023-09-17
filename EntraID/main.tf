beee9d6ac35f35d2a1cb19e8e101a9e5b7fc6181
Suryendu <suryendubbhattacharyya@gmail.com>
2023-09-17 10:34:10 +0200

    terraform init for ca policy change

---
+++ EntraID/main.tf
@@ -0,0 +1,28 @@
+resource "azuread_conditional_access_policy" "example" {
+  display_name = "Require MFA for high risk users"
+  state        = "enabledForReportingButNotEnforced"
+
+  conditions {
+    client_app_types    = ["all"]
+    sign_in_risk_levels = ["high"]
+    user_risk_levels    = ["high"]
+
+    applications {
+      included_applications = ["All"]
+      excluded_applications = []
+    }
+
+
+    users {
+      included_users = ["All"]
+      excluded_users = ["GuestsOrExternalUsers"]
+    }
+  }
+
+  grant_controls {
+    operator          = "OR"
+    built_in_controls = ["passwordChange"]
+  }
+
+
+}
---
+++ EntraID/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=2.23.0"
+    }
+  }
+}
+# Configure the Microsoft Azure Provider
+provider "azuread" {
+  use_oidc = true
+  features {}
+}
