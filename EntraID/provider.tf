42119bddccc50106140b0ca075dc7cd713a17d4c
Suryendu <suryendubbhattacharyya@gmail.com>
2023-09-17 11:09:32 +0200

    changed provider version

--- EntraID/provider.tf
+++ EntraID/provider.tf
@@ -2,12 +2,13 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "=2.23.0"
+      version = "=2.42.0"
     }
   }
 }
 # Configure the Microsoft Azure Provider
 provider "azuread" {
   use_oidc = true
+  features {}
   
 }
