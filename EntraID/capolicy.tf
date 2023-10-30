665207c4e61b6bad50115f100156751742758f14
Suryendu <suryendubbhattacharyya@gmail.com>
2023-10-30 16:49:35 +0100

    Update 2

--- EntraID/capolicy.tf
+++ EntraID/capolicy.tf
@@ -1,5 +1,5 @@
 resource "azuread_conditional_access_policy" "CA01-BlockHighRiskUsers" {
-  display_name = "Block high risk users"
+  display_name = "Block high risk users 1"
   state        = "enabledForReportingButNotEnforced"
 
   conditions {
