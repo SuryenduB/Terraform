c7ee247400fbb3b03020e762ee5cc0fb5d998853
Suryendu <suryendubbhattacharyya@gmail.com>
2023-10-31 11:48:29 +0100

    changed backend to Azure

--- EntraID/capolicy.tf
+++ EntraID/capolicy.tf
@@ -29,3 +29,37 @@ resource "azuread_conditional_access_policy" "CA200-Internals-BaseProtection-All
 
 
 }
+
+resource "azuread_conditional_access_policy" "CA202-Internals-IdentityProtection-AllApps-AnyPlatform-MFAandPWDforHighUserRisk" {
+  display_name = "CA202-Internals-IdentityProtection-AllApps-AnyPlatform-MFAandPWDforHighUserRisk"
+  state        = "enabledForReportingButNotEnforced"
+
+  conditions {
+    applications {
+      included_applications = ["All"]
+      excluded_applications = [data.azuread_service_principal.intune.client_id]
+    }
+    platforms {
+      included_platforms = ["all"]
+      
+    }
+    client_app_types    = ["all"]
+    user_risk_levels    = ["high"]
+
+
+    users {
+      included_groups = [azuread_group.internals.id]
+      excluded_groups = [azuread_group.breakglass.id,azuread_group.internals-BaseProtection-exclusion.id]
+    }
+    
+    
+    
+  }
+
+  grant_controls {
+    operator          = "AND"
+    built_in_controls = ["passwordChange","mfa"]
+  }
+
+
+}
--- EntraID/provider.tf
+++ EntraID/provider.tf
@@ -5,6 +5,18 @@ terraform {
       version = "=2.45.0"
     }
   }
+  
+  backend "azurerm" {
+    storage_account_name = "iamstgsa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+
+    # rather than defining this inline, the Access Key can also be sourced
+    # from an Environment Variable - more information is available below.
+    
+  }
+
+
 }
 # Configure the Microsoft Azure Provider
 provider "azuread" {
